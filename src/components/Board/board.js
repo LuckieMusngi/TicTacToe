4b861ccd0a0dfcbf05cd730b77c38ec7fd4189c4
Luckie Musngi <luckie.musngi@gmail.com>
2024-10-08 13:12:05 -0700

    finished board class

--- src/components/Board/board.js
+++ src/components/Board/board.js
@@ -6,16 +6,22 @@ class board {
     this.board = new Array(9).fill(" ");
   }
 
-  showRow(index) {
-    //! doesn't work since its a 1d array now
-    return this.board[index];
+  getRow(index) {
+    const rowIndex = index * 3;
+    return new Array(this.board[rowIndex], this.board[rowIndex + 1], this.board[rowIndex + 2]);
   }
 
-  playMove(player, index) {
-    if (this.board[i] != " ") {
+  playMove(player, i) {
+    if (player !== "X" && player !== "O") {
+
+    } 
+
+    if (this.board[i] !== " ") {
         //* ERROR
-        console.log("")
+        console.log("index is already occupied");
+        return;
     }
+    this.board[i] = player;
   }
 
   // returns 0 if no winner, 1 if player 1 wins and 2 if player 2 is winning
@@ -32,7 +38,7 @@ class board {
   hasWon(curPlayer) {
     // the index trios in which a player can win
     // a player wins if they have pieces in all 3 indexes of one of these arrays
-    const winningIndexes = [
+    const winCombs = [
         // rows
         [0, 1, 2],
         [3, 4, 5],
@@ -46,10 +52,10 @@ class board {
         [2, 4, 6],
     ];
 
-    for (const combination of winningIndexes) {
+    for (const combination of winCombs) {
         let found = true;
         for (const i of combination) {
-            if (this.board[i] != curPlayer) {
+            if (this.board[i] !== curPlayer) {
                 found = false;
             }
         }
@@ -62,10 +68,14 @@ class board {
   }
 
   toString() {
-    return "just think about it";
+    return this.board.toString();
   }
 
   getBoard() {
     return this.board;
+  }
+
+  getBoardHTML() {
+
   }
 }
